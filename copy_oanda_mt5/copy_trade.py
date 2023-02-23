6f4c9229b02ed631e19d7db4ec65cc53d1038c6b
MikeCorpTrade <mikaelmoreau747@gmail.com>
2023-02-23 22:58:18 +0100

    prepare the mt5 mapping

--- copy_oanda_mt5/copy_trade.py
+++ copy_oanda_mt5/copy_trade.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 # Oanda trade order details
-oanda_trade_order = {
+oanda_order = {
     "order": {
         "units": 1000,
         "instrument": "EUR_USD",
@@ -19,32 +19,38 @@ oanda_trade_order = {
     }
 }
 
-# Map the Oanda trade order to MT5 format
-mt5_trade_order = {
-    "volume": oanda_trade_order["order"]["units"],
-    "symbol": oanda_trade_order["order"]["instrument"],
-    "type": "OP_BUY" if oanda_trade_order["order"]["units"] > 0 else "OP_SELL",
-    "price": 0,
-    "sl": oanda_trade_order["order"]["stopLossOnFill"]["price"],
-    "tp": oanda_trade_order["order"]["takeProfitOnFill"]["price"],
-    "comment": "",
-    "magic": 123456,
-    "deviation": 200
-}
 
-# Place the trade order using MT5 API
-mt5_api_url = "https://api.mt5.com/v1/trade/order/place"
-mt5_api_key = "your_mt5_api_key"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + mt5_api_key
-}
-response = requests.post(mt5_api_url, headers=headers,
-                         data=json.dumps(mt5_trade_order))
+def map_order_mt5(trade):
+    # Map the Oanda trade order to MT5 format
+    mt5_trade_order = {
+        "volume": trade["units"],
+        "symbol": trade["instrument"],
+        "type": "OP_BUY" if trade["units"] > 0 else "OP_SELL",
+        "price": 0,
+        "sl": trade["stopLossOnFill"]["price"],
+        "tp": trade["takeProfitOnFill"]["price"],
+        "comment": "",
+        "magic": 123456,
+        "deviation": 200
+    }
 
-# Check the response status and retrieve the trade details
-if response.status_code == 200:
-    trade_details = response.json()
-    print("Trade placed successfully: ", trade_details)
-else:
-    print("Failed to place trade: ", response.text)
+    # Place the trade order using MT5 API
+    mt5_api_url = "https://api.mt5.com/v1/trade/order/place"
+    mt5_api_key = "your_mt5_api_key"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + mt5_api_key
+    }
+    response = requests.post(mt5_api_url, headers=headers,
+                             data=json.dumps(mt5_trade_order))
+
+    # Check the response status and retrieve the trade details
+    if response.status_code == 200:
+        trade_details = response.json()
+        print("Trade placed successfully: ", trade_details)
+    else:
+        print("Failed to place trade: ", response.text)
+
+
+if __name__ == "__main__":
+    map_order_mt5(trade=oanda_order)
