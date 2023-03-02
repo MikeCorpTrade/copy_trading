93a409f02c2f34213083465a67004036bd5e11c6
MikeCorpTrade <mikaelmoreau747@gmail.com>
2023-03-02 19:03:50 +0100

    mapped oanda to mt5

--- copy_oanda_mt5/copy_trade.py
+++ copy_oanda_mt5/copy_trade.py
@@ -16,19 +16,31 @@ oanda_order = {
         }
     }
 
+mt5_coeff = 9.80E-6
+
+oanda_mt5_symbols = {
+    "EUR_USD": "EURUSD"
+}
+
 
 def map_order_oanda_to_mt5(trade):
     # Map the Oanda trade order to MT5 format
+
+    mt5_SYMBOL = oanda_mt5_symbols[trade["instrument"]]
+
     mt5_trade_order = {
-        "volume": trade["units"],
-        "symbol": trade["instrument"],
+        "action": mt5.TRADE_ACTION_DEAL,
+        "volume": round(trade["units"]*mt5_coeff, 2),
+        "symbol": mt5_SYMBOL,
         "type": mt5.ORDER_TYPE_BUY if trade["units"] > 0 else mt5.ORDER_TYPE_SELL,
-        "price": 0,
+        "price": mt5.symbol_info_tick(mt5_SYMBOL).ask,
         "sl": trade["stopLossOnFill"]["price"],
         "tp": trade["takeProfitOnFill"]["price"],
         "comment": "",
         "magic": 123456,
-        "deviation": 200
+        "deviation": 200,
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
     }
     return mt5_trade_order
 
